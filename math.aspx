a7291969b15b1321f2c566f4283273c60e125907
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-09-04 23:56:52 +0545

    Add files via upload

---
+++ math.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="math.aspx.cs" Inherits="mathcalldll.math" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbladd" runat="server" Text="First Number"></asp:Label>
+            <asp:TextBox ID="txtfirst" runat="server" Text=""></asp:TextBox><br />
+            <asp:Label ID="lbladd1" runat="server" Text="Second Number"></asp:Label>
+            <asp:TextBox ID="txtsecond" runat="server" Text=""></asp:TextBox><br />
+            <asp:Label ID="lbltotal" runat="server" Text="Result"></asp:Label>
+            <asp:TextBox ID="txttotal" runat="server" Text=""></asp:TextBox>
+            <br />
+            
+        </div>
+
+        <div>
+            <asp:Button ID="btnsum" runat="server" Text="ADD" OnClick="btnsum_Click" />
+            <asp:Label ID="lblmsg" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ math.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using addnumdll;
+
+namespace mathcalldll
+{
+    public partial class math : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsum_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            //string str;
+            addnumdll.Class1 aa = new addnumdll.Class1();
+            a = Convert.ToInt32(txtfirst.Text);
+            b = Convert.ToInt32(txtsecond.Text);
+            Response.Write( "Total:"+"\t" +aa.substract(a, b));
+            int total;
+            //total = Convert.ToInt32(txttotal.Text);
+            Response.Write("Substract:" + aa.substract(a, b));
+            total = aa.substract(a, b);
+            lblmsg.Text = Convert.ToInt32(total).ToString();
+            txttotal.Text = Convert.ToInt32(total).ToString();
+
+            //txttotal.Text = (aa.substract(a, b));
+
+        }
+    }
+}
